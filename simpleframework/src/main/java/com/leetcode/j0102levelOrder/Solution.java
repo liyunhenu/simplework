b415236c51064ef8368e0aaa77ca361e80a9809f
liyun1 <liyunhenu@163.com>
2021-10-05 16:34:22 +0800

    leetcode 0102 广度优先遍历的一种实现

--- simpleframework/src/main/java/com/leetcode/j0102levelOrder/Solution.java
+++ simpleframework/src/main/java/com/leetcode/j0102levelOrder/Solution.java
@@ -6,17 +6,35 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Solution {
 
     public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        queue.add(new Pair<>(root, 0));
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, Integer> pair = queue.poll();
+            TreeNode node = pair.getKey();
+            Integer depth = pair.getValue();
+            if (depth == res.size()) {
+                res.add(new ArrayList<>());
+            }
+            ArrayList list = (ArrayList) res.get(depth);
+            list.add(node.val);
+            if (node.left != null) {
+                queue.add(new Pair<>(node.left, depth + 1));
+            }
+            if (node.right != null) {
+                queue.add(new Pair<>(node.right, depth + 1));
+            }
 
-        return null;
+        }
+        return res;
     }
 
-
-    public static void main(String[] args) {
-        List<List<Integer>> list=new ArrayList<>();
-        List<Pair<TreeNode,Integer>> queue=new LinkedList<>();
-    }
 }
