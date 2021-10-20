d41920158b125106b7f2510f1b0b6aaa3001442e
liyun <liyunhenu@163.com>
2021-10-20 18:36:36 +0800

    深度遍历回溯

--- simpleframework/src/main/java/com/leetcode/j0126findLadders/Solution.java
+++ simpleframework/src/main/java/com/leetcode/j0126findLadders/Solution.java
@@ -1,13 +1,19 @@
-package com.leetcode.j0127ladderLength;
+package com.leetcode.j0126findLadders;
 
 import javafx.util.Pair;
 
 import java.util.*;
 
 public class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+    //dfs重构树  每层有几个元素   每个元素从哪几个元素获得
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> res = new ArrayList<>();
+        Map<Integer, List<Integer>> from = new HashMap<>();
+        boolean isExist = false;
+
         if (!wordList.contains(endWord)) {
-            return 0;
+            return res;
         }
         if (!wordList.contains(beginWord)) {
             wordList.add(beginWord);
@@ -33,9 +39,13 @@ public class Solution {
             int position = (int) pair.getKey();
             int depth = (int) pair.getValue();
             for (int i = 0; i < wordList.size(); i++) {
+
                 if (visited[i] != 1 && ladderVector[position][i] == 1) {
+                    from.putIfAbsent(i, new ArrayList<>());
+                    from.get(i).add(position);
                     if (wordList.get(i) == endWord) {
-                        return depth + 1;
+                        isExist = true;//多个路径到达不退出循环，保证所有可能相同长度路径都能被遍历到
+                        //break;
                     } else {
                         queue.offer(new Pair<>(i, depth + 1));
                         visited[i] = 1;
@@ -44,9 +54,14 @@ public class Solution {
             }
 
         }
-        return 0;
-    }
+        if (isExist) {
 
+
+        }
+
+
+        return res;
+    }
 
     public boolean isLadder(String str, String str1) {
         if (str.length() != str1.length()) {
@@ -67,9 +82,15 @@ public class Solution {
         List<String> wordList = new ArrayList<>();
         Collections.addAll(wordList, strs);
         Solution solution = new Solution();
-        System.out.println(solution.ladderLength(beginWord, endWord, wordList));
-
+        List<List<String>> result = solution.findLadders(beginWord, endWord, wordList);
+        for (List list : result) {
+            System.out.print("[");
+            for (Object object : list
+            ) {
+                System.out.print(object + "->");
+            }
+            System.out.print("null]");
+            System.out.println();
+        }
     }
-
-
 }
