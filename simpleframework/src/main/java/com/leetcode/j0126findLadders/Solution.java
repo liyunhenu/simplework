2c291c2319b8060c5c933ffe84828776eb5cd3fa
liyun12 <liyun12.cmbc.com.cn>
2021-10-22 21:26:51 +0800

    层序遍历+深度优先遍历回溯，未完成

--- simpleframework/src/main/java/com/leetcode/j0126findLadders/Solution.java
+++ simpleframework/src/main/java/com/leetcode/j0126findLadders/Solution.java
@@ -34,6 +34,7 @@ public class Solution {
         queue.offer(new Pair<>(k, 1));
         int[] visited = new int[wordList.size()];
         visited[k] = 1;
+        Integer minDepth=0;
         while (!queue.isEmpty()) {
             Pair pair = queue.poll();
             int position = (int) pair.getKey();
@@ -41,26 +42,51 @@ public class Solution {
             for (int i = 0; i < wordList.size(); i++) {
 
                 if (visited[i] != 1 && ladderVector[position][i] == 1) {
-                    from.putIfAbsent(i, new ArrayList<>());
-                    from.get(i).add(position);
+
                     if (wordList.get(i) == endWord) {
                         isExist = true;//多个路径到达不退出循环，保证所有可能相同长度路径都能被遍历到
-                        //break;
+                        if(minDepth==0){
+                            minDepth=depth+1;
+                        }
+                        if(depth+1>minDepth){
+                            continue;//如果不是最短路径，跳过
+                        }
                     } else {
                         queue.offer(new Pair<>(i, depth + 1));
                         visited[i] = 1;
                     }
+                    from.putIfAbsent(i, new ArrayList<>());
+                    from.get(i).add(position);
                 }
             }
 
         }
         if (isExist) {
-
-
+            Deque<String> path = new LinkedList<>();
+            dfs(wordList,endWord,beginWord,path,from,res);
         }
 
 
         return res;
+    }
+
+    private void dfs(List<String> wordList, String beginWord, String endWord, Deque<String> path, Map<Integer, List<Integer>> from, List<List<String>> res) {
+        int beginIndex=wordList.indexOf(beginWord);
+        int endIndex=wordList.indexOf(endWord);
+        //List<String> queue=new LinkedList<>();
+        path.addFirst(beginWord);
+        List fromNow=from.get(beginIndex);
+        for (int i = 0; i < fromNow.size(); i++) {
+            if((int)fromNow.get(i)==endIndex){
+                path.addFirst(endWord);
+                res.add(new ArrayList<>(path));//当前路径找到了
+                return;
+            }
+            dfs(wordList,wordList.get((int)fromNow.get(i)),endWord, path, from,res);
+        }
+        path.remove(beginWord);
+
+
     }
 
     public boolean isLadder(String str, String str1) {
